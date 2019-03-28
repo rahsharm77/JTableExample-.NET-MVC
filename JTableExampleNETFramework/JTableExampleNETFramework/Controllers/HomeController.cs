2d36d6a1a325435f72f6258713bde7f0f0bf4fe0
rahsharm77 <43404256+rahsharm77@users.noreply.github.com>
2019-03-28 17:40:07 +0530

    Initial Commit for JTable Plugin run in .NET MVC 4.5
    Initial Commit

---
+++ JTableExampleNETFramework/JTableExampleNETFramework/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using JTableExampleNETFramework.Models;
+using System.Web.Mvc;
+
+namespace JTableExampleNETFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Added as an example to populate our dummy list
+        [HttpPost]
+        public JsonResult GetItems()
+        {
+            try
+            {
+                //Add to our list
+                List<Student> studentList = new List<Student>()
+                {
+                  new Student(){ StudentID=1, StudentName="Bill"},
+                  new Student(){ StudentID=2, StudentName="Steve"},
+                  new Student(){ StudentID=3, StudentName="Ram"},
+                  new Student(){ StudentID=4, StudentName="Moin"}
+                };
+
+                //var json = JsonConvert.SerializeObject(studentList);
+                return Json(new { Result = "OK", Records = studentList, TotalRecordCount = studentList.Count });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+        }
+    }
+}
---
+++ JTableExampleNETFramework/JTableExampleNETFramework/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace JTableExampleNETFramework.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+}
